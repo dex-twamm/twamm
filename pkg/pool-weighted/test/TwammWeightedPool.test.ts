1e1dab7164299b47c388ab4feb363c1c9c779f31
codesherpa <codesherpa.xs@gmail.com>
2022-06-02 16:11:28 +0530

    + Added BaseWeightedPool tests for TWAMM + Cleaned up unnecessary tests

--- pkg/pool-weighted/test/TwammWeightedPool.test.ts
+++ pkg/pool-weighted/test/TwammWeightedPool.test.ts
@@ -8,6 +8,8 @@ import TokenList from '@balancer-labs/v2-helpers/src/models/tokens/TokenList';
 import WeightedPool from '@balancer-labs/v2-helpers/src/models/pools/weighted/WeightedPool';
 import { WeightedPoolType } from '@balancer-labs/v2-helpers/src/models/pools/weighted/types';
 
+import { itBehavesAsWeightedPool } from './BaseWeightedPool.behavior';
+
 import { range } from 'lodash';
 
 async function moveForwardNBlocks(n: number) {
@@ -18,56 +20,30 @@ async function moveForwardNBlocks(n: number) {
 
 // TODO(codesherpa): Add real tests. Current tests are duplicate of WeightedPool tests
 describe('TwammWeightedPool', function () {
-  let owner: SignerWithAddress, other: SignerWithAddress;
+  
+  describe('long term order tests', () => {
 
-  before('setup signers', async () => {
-    [, owner, other] = await ethers.getSigners();
-  });
+    let owner: SignerWithAddress, other: SignerWithAddress;
 
-  const MAX_TOKENS = 2;
+    before('setup signers', async () => {
+      [, owner, other] = await ethers.getSigners();
+    });
 
-  let allTokens: TokenList, tokens: TokenList;
+    const MAX_TOKENS = 2;
 
-  let sender: SignerWithAddress;
-  let pool: WeightedPool;
-  const weights = [fp(0.5), fp(0.5)];
-  // 1 token A = 4 token B
-  const initialBalances = [fp(100.0), fp(400.0)];
+    let allTokens: TokenList, tokens: TokenList;
 
-  sharedBeforeEach('deploy tokens', async () => {
-    allTokens = await TokenList.create(MAX_TOKENS + 1, { sorted: true });
-    tokens = allTokens.subset(2);
-    await tokens.mint({ to: [owner, other], amount: fp(100000.0) });
-  });
-
-  describe('weights and scaling factors', () => {
-    for (const numTokens of range(2, MAX_TOKENS + 1)) {
-      context(`with ${numTokens} tokens`, () => {
-        let pool: WeightedPool;
-        let tokens: TokenList;
-
-        sharedBeforeEach('deploy pool', async () => {
-          tokens = allTokens.subset(numTokens);
-
-          pool = await WeightedPool.create({
-            poolType: WeightedPoolType.LIQUIDITY_BOOTSTRAPPING_POOL,
-            tokens,
-            weights: weights.slice(0, numTokens),
-          });
-        });
-
-        it('sets scaling factors', async () => {
-          const poolScalingFactors = await pool.getScalingFactors();
-          const tokenScalingFactors = tokens.map((token) => fp(10 ** (18 - token.decimals)));
-
-          expect(poolScalingFactors).to.deep.equal(tokenScalingFactors);
-        });
-      });
-    }
-  });
-
-  describe('with valid creation parameters', () => {
+    let sender: SignerWithAddress;
     let pool: WeightedPool;
+    const weights = [fp(0.5), fp(0.5)];
+    // 1 token A = 4 token B
+    const initialBalances = [fp(100.0), fp(400.0)];
+
+    sharedBeforeEach('deploy tokens', async () => {
+      allTokens = await TokenList.create(MAX_TOKENS + 1, { sorted: true });
+      tokens = allTokens.subset(2);
+      await tokens.mint({ to: [owner, other], amount: fp(100000.0) });
+    });
 
     context('when initialized with swaps enabled', () => {
       sharedBeforeEach('deploy pool', async () => {
@@ -94,15 +70,9 @@ describe('TwammWeightedPool', function () {
             await tokens.approve({ from: other, to: await pool.getVault() });
           });
 
-          it('can execute Long Term Order', async () => {
-            
-            const longTermOrder = await pool.placeLongTermOrder({
-              from: other,
-              amountIn: fp(1.0),
-              tokenInIndex: 0,
-              tokenOutIndex: 1,
-              numberOfBlockIntervals: 10,
-            });
+          it('can execute one-way Long Term Order', async () => {
+            await tokens.approve({ from: other, to: await pool.getVault() });
+            let longTermOrder = await pool.placeLongTermOrder({ from: other, amountIn: fp(1.0), tokenInIndex: 0, tokenOutIndex: 1, numberOfBlockIntervals: 10 });
 
             // Move forward 80 blocks with one swap after every 20 blocks.
             for (let j = 0; j < 4; j++) {
@@ -115,10 +85,58 @@ describe('TwammWeightedPool', function () {
 
             const withdrawResult = await pool.withdrawLongTermOrder({ orderId: 0, from: other });
             expect(withdrawResult.amountsOut[0]).to.be.equal(fp(0));
-            expect(withdrawResult.amountsOut[1]).to.be.gte(fp(3.9));
+            expect(withdrawResult.amountsOut[1]).to.be.gte(fp(3.95));
+          });
+
+          it('can cancel one-way Long Term Order', async () => {
+            await tokens.approve({ from: other, to: await pool.getVault() });
+            let longTermOrder = await pool.placeLongTermOrder({ from: other, amountIn: fp(1.0), tokenInIndex: 0, tokenOutIndex: 1, numberOfBlockIntervals: 10 });
+
+            // Move forward 40 blocks with one swap after every 10 blocks.
+            for (let j = 0; j < 4; j++) {
+              await moveForwardNBlocks(10);
+              const result = await pool.swapGivenIn({ in: 0, out: 1, amount: fp(0.1) });
+            }
+
+            // Move forward to mid of the long term order duration. I.e., t+50 blocks.
+            await moveForwardNBlocks(10);
+
+            const cancelResult = await pool.cancelLongTermOrder({ orderId: 0, from: other });
+            expect(cancelResult.amountsOut[0]).to.be.gte(fp(0.4));
+            expect(cancelResult.amountsOut[1]).to.be.lte(fp(2.2));
+          });
+
+          it('can execute two-way Long Term Order', async () => {
+            await tokens.approve({ from: other, to: await pool.getVault() });
+            let longTermOrder1 = await pool.placeLongTermOrder({ from: other, amountIn: fp(1.0), tokenInIndex: 0, tokenOutIndex: 1, numberOfBlockIntervals: 10 });
+            let longTermOrder2 = await pool.placeLongTermOrder({ from: other, amountIn: fp(10.0), tokenInIndex: 1, tokenOutIndex: 0, numberOfBlockIntervals: 10 });
+
+            // Move forward 80 blocks with one swap after every 20 blocks.
+            for (let j = 0; j < 4; j++) {
+              await moveForwardNBlocks(20);
+              var result = await pool.swapGivenIn({ in: 0, out: 1, amount: fp(0.1) });
+            }
+
+            // Move forward to end of expiry block of the long term order.
+            await moveForwardNBlocks(20);
+
+            var withdrawResult = await pool.withdrawLongTermOrder({ orderId: 0, from: other });
+            var withdrawResult1 = await pool.withdrawLongTermOrder({ orderId: 1, from: other });
+            expect(withdrawResult.amountsOut[0]).to.be.equal(fp(0));
+            expect(withdrawResult.amountsOut[1]).to.be.gte(fp(3.95));
+
+            expect(withdrawResult1.amountsOut[0]).to.be.gte(fp(2.45));
+            expect(withdrawResult1.amountsOut[1]).to.be.equal(fp(0));
           });
         });
       });
+    });
+  });
+
+  describe('BaseWeightedPool tests', function () {
+    context('for a 2 token pool', () => {
+      // Should behave as basic weighted pool if no long term orders are placed.
+      itBehavesAsWeightedPool(2, WeightedPoolType.TWAMM_WEIGHTED_POOL);
     });
   });
 });
