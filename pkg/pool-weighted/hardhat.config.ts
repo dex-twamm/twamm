180f93c0afe7bfada3def498f1d4f11f0883e673
codesherpa <codesherpa.xs@gmail.com>
2022-11-30 16:16:50 +0530

    Try hardhat config fix

--- pkg/pool-weighted/hardhat.config.ts
+++ pkg/pool-weighted/hardhat.config.ts
@@ -12,7 +12,23 @@ import { name } from './package.json';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const { GOERLI_API_URL, RINKEBY_API_URL, MAINNET_API_URL, ETHERSCAN_KEY, PRIVATE_KEY , MAINNET_PRIVATE_KEY} = process.env;
+const {
+  GOERLI_API_URL,
+  RINKEBY_API_URL,
+  MAINNET_API_URL,
+  ETHERSCAN_KEY,
+  PRIVATE_KEY,
+  MAINNET_PRIVATE_KEY,
+} = {
+  // Dummy values for test
+  GOERLI_API_URL: "https://eth-rinkeby.alchemyapi.io/v2/123456789abcdefghijk_lmn-oprstuv",
+  RINKEBY_API_URL: "https://eth-rinkeby.alchemyapi.io/v2/123456789abcdefghijk_lmn-oprstuv",
+  MAINNET_API_URL: "https://eth-rinkeby.alchemyapi.io/v2/123456789abcdefghijk_lmn-oprstuv",
+  ETHERSCAN_KEY: "1234567890abcdef1234567890abcdef12",
+  PRIVATE_KEY: "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+  MAINNET_PRIVATE_KEY: "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+  ...process.env,
+};
 
 import { task } from 'hardhat/config';
 import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
